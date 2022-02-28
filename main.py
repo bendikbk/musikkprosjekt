fccf4452b10c7c1a53229ab2ae485a4715d02252
bendikbk <bendik.karlsen@hotmail.com>
2022-02-28 11:25:23 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from playsound import playsound
+import os, random
+
+
+
+def ps():
+    playsound("musikkprosjekt/Justin Bieber-Baby", block=False)
+
+    ps()
+
+    while True:
+       stopp = input("Hvem er dette")
+       if stopp =="justin bieber":
